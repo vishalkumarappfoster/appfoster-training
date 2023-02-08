15ee9176ba7f96b6693830250075c61a00c3a9b3
vishal kumar <vishal.kumar@appfoster.com>
2023-02-08 18:16:05 +0530

    logic-update

--- task-8/script/script.js
+++ task-8/script/script.js
@@ -1,25 +1,22 @@
+const colors = ["red", "3AE9CE", "#ffcc00", "blue", "#ffd3e2", "pink"];
+
 function split() {
-    const input = document.getElementById("inputNumber").value;
-    const splitCount = document.getElementById("splitNumber").value;
-    const result = document.getElementById("result");
-    result.innerHTML = "";
-  
-    let remaining = input;
-    for (let i = 0; i < splitCount; i++) {
-      const eachSplit = Math.min(Math.ceil(remaining / (splitCount - i)), remaining);
-      remaining -= eachSplit;
-      
-      const div = document.createElement("div");
-      div.innerHTML = eachSplit;
-      div.style.width = `${100 / splitCount}%`;
-      
-      if (eachSplit >= 5) {
-        div.classList.add(`highlight${(i % 4) + 1}`);
-      } else {
-        div.classList.add("lowlight");
-      }
-      
-      result.appendChild(div);
-    }
+  const input = document.getElementById("inputNumber").value;
+  const splitCount = document.getElementById("splitNumber").value;
+  const result = document.getElementById("result");
+  result.innerHTML = "";
+
+  let remaining = input;
+  for (let i = 0; i < splitCount; i++) {
+    const eachSplit = Math.floor(remaining / (splitCount - i));
+    remaining -= eachSplit;
+
+    const div = document.createElement("div");
+    div.innerHTML = eachSplit;
+    div.style.display = "inline-block";
+    div.style.width = `${100 / splitCount}%`;
+    div.style.backgroundColor = colors[i % colors.length];
+
+    result.appendChild(div);
   }
-  
+}
