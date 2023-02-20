0e24246641f8feef2e40c8e37f91922fe01112e4
vishal kumar <vishal.kumar@appfoster.com>
2023-02-20 17:06:34 +0530

    jquery added to the button

--- task-10-javascript-async/script.js
+++ task-10-javascript-async/script.js
@@ -1,4 +1,5 @@
-async function showUsers() {
+$(document).ready(function () {
+  async function showUsers() {
     try {
       const data = await fetch("https://gorest.co.in/public/v2/users");
       const objectData = await data.json();
@@ -6,31 +7,34 @@ async function showUsers() {
       objectData.map((values) => {
         tableData += `<tr>
           <td>${values.name}</td>
-          <td><button class="btn btn-primary" data-toggle="modal" data-target="#userModal" onclick="displayDetails('${values.id}')">View More</button></td>
+          <td><button class="btn btn-primary view-more-detail-btn" data-id="${values.id}" data-toggle="modal" data-target="#userModal">View More</button></td>
         </tr>`;
       });
-      document.getElementById("tableBody").innerHTML = tableData;
+      $("#tableBody").html(tableData);
     } catch (err) {
       console.log("error occured!", err);
     } finally {
       console.log("done fine");
     }
   }
-  showUsers();
-  
   async function displayDetails(id) {
     try {
-      const data = await fetch("https://gorest.co.in/public/v2/users/" + id);
+      const data = await fetch(`https://gorest.co.in/public/v2/users/${id}`);
       const objectData = await data.json();
-      document.getElementById("name").innerHTML = objectData.name;
-      document.getElementById("email").innerHTML = objectData.email;
-      document.getElementById("gender").innerHTML = objectData.gender;
+      $("#name").html(objectData.name);
+      $("#email").html(objectData.email);
+      $("#gender").html(objectData.gender);
     } catch (err) {
       console.log("error occured!", err);
     } finally {
       console.log("done fine");
     }
   }
-  
- 
-  
+
+  $(document).on("click", ".view-more-detail-btn", function () {
+    const userId = $(this).data("id");
+    displayDetails(userId);
+  });
+
+  showUsers();
+});
