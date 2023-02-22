660427a48eb473f664a49fb64018ca473ca99c97
vishal kumar <vishal.kumar@appfoster.com>
2023-02-22 17:53:15 +0530

    Starting Laravel Project

---
+++ task-13-laravel/src/database/migrations/2023_02_22_121449_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',60);
+            $table->string('email',100);
+            $table->enum('gender', ["M", "F", "O"])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
